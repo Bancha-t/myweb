0ea42523c07f36df01bcfe1dd7713178bb19c116
Bancha-t <gmtv7887@gmail.com>
2024-09-28 17:52:38 +0700

    updata

--- mywebfortest-main/src/pages/Showbook/AllBook.tsx
+++ mywebfortest-main/src/pages/Showbook/AllBook.tsx
@@ -1,21 +1,26 @@
-import React, { useState, CSSProperties } from 'react';
-import Header from '../../components/Header'
-import SearchBar from '../../components/SearchBar'
+import React from 'react';
+import Header from '../../components/Header';
+import SearchBar from '../../components/SearchBar';
+import Cart from '../../components/Cart';
+import { useCart } from '../../contexts/CartContext';
 
-const styles: { [key: string]: CSSProperties } = {
-  Tital:{
+const styles: { [key: string]: React.CSSProperties } = {
+  Tital: {
     fontSize: '20px'
   }
-}
+};
 
 function AllBook() {
+  const { isCartOpen, toggleCart, cartItems, updateQuantity } = useCart();
+
   return (
     <>
-        <Header />
-        <SearchBar />
-        <div style={styles.Tital}>All Book</div>
+      <Header />
+      <SearchBar />
+      <div style={styles.Tital}>All Book</div>
+      <Cart isOpen={isCartOpen} toggleCart={toggleCart} items={cartItems} updateQuantity={updateQuantity} />
     </>
-  )
+  );
 }
 
-export default AllBook
+export default AllBook;
--- mywebfortest-main/src/pages/Showbook/NewBook.tsx
+++ mywebfortest-main/src/pages/Showbook/NewBook.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Header from '../../components/Header'
-import SearchBar from '../../components/SearchBar'
+import React from 'react';
+import Header from '../../components/Header';
+import SearchBar from '../../components/SearchBar';
+import Cart from '../../components/Cart';
+import { useCart } from '../../contexts/CartContext';
 
 function NewBook() {
+  const { isCartOpen, toggleCart, cartItems, updateQuantity } = useCart();
+
   return (
     <>
-        <Header />
-        <SearchBar />
+      <Header />
+      <SearchBar />
+      <div style={{ fontSize: '20px' }}>New Book</div>
+      <Cart isOpen={isCartOpen} toggleCart={toggleCart} items={cartItems} updateQuantity={updateQuantity} />
     </>
-  )
+  );
 }
 
-export default NewBook
+export default NewBook;
